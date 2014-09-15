765c863822429e578b7acb1670bdf97f106fc779
Zebedee Pedersen <zeb@zebpedersen.com>
2014-09-15 18:11:34 +0100

    added node-osc code

--- node/app.js
+++ node/app.js
@@ -16,11 +16,79 @@ var app = express();
 app.use(express.static(__dirname + '/public'));   // set the static files location /public/img will be /img for users
 app.use(morgan('dev'));           //
 
-https.createServer(options, app).listen(port, function(){
+var server = https.createServer(options, app).listen(port, function(){
     console.log("Express server listening on port " + port);
 });
+
+var io = require('socket.io')(server);
+
+
+var OSC_HOST = '0.0.0.0',
+	OSC_PORT = 3333;
+var osc = require('node-osc');
+
+var SOCKET_CLIENT = null;
+console.log('Starting OSC server on ' + OSC_HOST + ':' + OSC_PORT)
+var oscServer = new osc.Server(OSC_PORT, OSC_HOST);
+
+//	Get the socket client
+io.on('connection', function (socket)	{
+	console.log('Client connected');
+	SOCKET_CLIENT = socket;
+});
+
+io.on('disconnect', function (socker)	{
+	console.log('Client disconnected');
+	if (socket == SOCKET_CLIENT)	{
+		SOCKET_CLIENT = null;
+	}
+});
+
+//	Forward incoming OSC data
+var LATEST_DB, LATEST_PITCH;
+	LATEST_DB = LATEST_DB = 0;
+
+oscServer.on('message', function (msg, rinfo){
+	var packet_complete = false;
+
+	//	handle any malformed messages
+	if (msg.length <= 1)	{
+		return;
+	}
+
+	if (msg[0] == 'db')	{
+		LATEST_DB = msg[1];
+		packet_complete = false;
+	}	
+	else if (msg[0] == 'pitch') 	{
+		LATEST_PITCH = msg[1];
+		packet_complete = true;
+	}
+	
+	if (!packet_complete)	return;
+	
+	if (SOCKET_CLIENT != null)	{
+
+		SOCKET_CLIENT.emit('pitchAmpPacket', {
+			pitch: 	LATEST_PITCH,
+			amp: 	LATEST_DB
+		});
+	}
+});
+
+
+
+
+
+
+
 
 app.get('/', function (req, res) {
     res.writeHead(200);
     res.end("hello world\n");
 });
+
+
+//	OSC
+var OSC_HOST = '0.0.0.0',
+	OSC_PORT = 3333;
--- node/public/voice/js/main.js
+++ node/public/voice/js/main.js
@@ -6,7 +6,7 @@ var keyIsDown=false;
 $(document).ready(function() {
 	initRecognition();
 	initButtons();
-	initAudioStream();
+	// initAudioStream();
 
 	window.addEventListener("keypress", kd, false);
 	window.addEventListener("keyup", ku, false);
@@ -88,75 +88,6 @@ function startRecognition () {
 	recognition.start();
 };
 
-
-navigator.getUserMedia = navigator.webkitGetUserMedia 
-var audioCtx = new (window.webkitAudioContext)();
-var voiceSelect = document.getElementById("voice");
-var source;
-var stream;
-var analyser = audioCtx.createAnalyser();
-var gainNode = audioCtx.createGain();
-var drawVisual;
-var buflen = 2048;
-var buf = new Uint8Array( buflen );
-//main block for doing the audio recording
-function initAudioStream(){
-	if (navigator.getUserMedia) {
-		console.log('getUserMedia supported.');
-		navigator.getUserMedia (
-	  // constraints - only audio needed for this app
-	  {
-	  	audio: true
-	  },
-
-	  function(stream) {
-	  	source = audioCtx.createMediaStreamSource(stream);
-	  	source.connect(analyser);
-	  	gainNode.connect(audioCtx.destination);
-	  	visualize();
-
-	  	var mediaStreamSource = audioContext.createMediaStreamSource(stream);
-
-    // Connect it to the destination.
-    analyser = audioContext.createAnalyser();
-    analyser.fftSize = 2048;
-    mediaStreamSource.connect( analyser );
-    updatePitch();
-
-
-	  	// toggleLiveInput()
-	  },
-	  function(err) {
-	  	console.log('The following gUM error occured: ' + err);
-	  }
-	  );
-	} else {
-		console.log('getUserMedia not supported on your browser!');
-	}
-}
-
-
-
-
-function visualize() {
-	analyser.fftSize = 256;
-	var bufferLength = analyser.frequencyBinCount;
-	console.log(bufferLength);
-	var dataArray = new Uint8Array(bufferLength);
-	function draw() {
-		drawVisual = requestAnimationFrame(draw);
-		analyser.getByteFrequencyData(dataArray);
-		var average=0;
-		for(var i = 0; i < bufferLength; i++) {
-			average+=dataArray[i];
-		}
-		average=average/bufferLength;
-		$(".volume").css({top:100-(average*1)})
-
-	};
-	draw();
-}
-
 function newMessage(){
 	console.log($("textarea").text())
 	$(".message").removeClass("active");
@@ -169,134 +100,256 @@ function newMessage(){
 	if($(".messages").height()>1000){
 		TweenMax.to(".messages",0.5,{top:"-=100"})
 	}
-
 }
 
-/*
-Pitch Detection
-*/
+var Analysis = function ()	{
+	var self = this;
+	self.PACKETS = [];
+	self.SAMPLE_LENGTH = 100;
+	self.AMP = 0;
+	self.PITCH = 0;
+	self.socket = io.connect('localhost:8080');
 
+	self.socket.on('pitchAmpPacket', function(data)	{
+		self.pushNewPacket(data);
+		self.calculateAverages();
+		self.printAverages();
+	});
 
-var audioContext = new AudioContext();
-var isPlaying = false;
-var sourceNode = null;
-// var analyser = null;
-var theBuffer = null;
-var pitch=0;
-//
-var rafID = null;
-var buflen = 2048;
-var buf = new Uint8Array( buflen );
-var noteStrings = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+	self.$pitch = $('#pitch');
+	self.$amp = $('#amp');
+};
 
-
-function gotStream(stream) {
-    // Create an AudioNode from the stream.
-    var mediaStreamSource = audioContext.createMediaStreamSource(stream);
-
-    // Connect it to the destination.
-    analyser = audioContext.createAnalyser();
-    analyser.fftSize = 2048;
-    mediaStreamSource.connect( analyser );
-    updatePitch();
-}
-
-
-
-function toggleLiveInput() {
-	if (isPlaying) {
-        //stop playing and return
-        sourceNode.stop( now );
-        sourceNode = null;
-        analyser = null;
-        isPlaying = false;
-        if (!window.cancelAnimationFrame)
-        	window.cancelAnimationFrame = window.webkitCancelAnimationFrame;
-        window.cancelAnimationFrame( rafID );
-    }
-    getUserMedia({audio:true}, gotStream);
-}
-
-
-
-function noteFromPitch( frequency ) {
-	var noteNum = 12 * (Math.log( frequency / 440 )/Math.log(2) );
-	return Math.round( noteNum ) + 69;
-}
-
-function frequencyFromNoteNumber( note ) {
-	return 440 * Math.pow(2,(note-69)/12);
-}
-
-function centsOffFromPitch( frequency, note ) {
-	return Math.floor( 1200 * Math.log( frequency / frequencyFromNoteNumber( note ))/Math.log(2) );
-}
-
-
-
-function autoCorrelate( buf, sampleRate ) {
-	var MIN_SAMPLES = 4;	// corresponds to an 11kHz signal
-	var MAX_SAMPLES = 1000; // corresponds to a 44Hz signal
-	var SIZE = 1000;
-	var best_offset = -1;
-	var best_correlation = 0;
-	var rms = 0;
-	var foundGoodCorrelation = false;
-
-	if (buf.length < (SIZE + MAX_SAMPLES - MIN_SAMPLES))
-		return -1;  // Not enough data
-
-	for (var i=0;i<SIZE;i++) {
-		var val = (buf[i] - 128)/128;
-		rms += val*val;
+Analysis.prototype.pushNewPacket = function(packet) {
+	this.PACKETS.push(packet);
+	if (this.PACKETS.length > this.SAMPLE_LENGTH)	{
+		this.PACKETS = this.PACKETS.slice(1, this.PACKETS.length - 1);
 	}
-	rms = Math.sqrt(rms/SIZE);
-	if (rms<0.01)
-		return -1;
+};
 
-	var lastCorrelation=1;
-	for (var offset = MIN_SAMPLES; offset <= MAX_SAMPLES; offset++) {
-		var correlation = 0;
-
-		for (var i=0; i<SIZE; i++) {
-			correlation += Math.abs(((buf[i] - 128)/128)-((buf[i+offset] - 128)/128));
-		}
-		correlation = 1 - (correlation/SIZE);
-		if ((correlation>0.9) && (correlation > lastCorrelation))
-			foundGoodCorrelation = true;
-		else if (foundGoodCorrelation) {
-			// short-circuit - we found a good correlation, then a bad one, so we'd just be seeing copies from here.
-			return sampleRate/best_offset;
-		}
-		lastCorrelation = correlation;
-		if (correlation > best_correlation) {
-			best_correlation = correlation;
-			best_offset = offset;
-		}
+Analysis.prototype.calculateAverages = function ()	{
+	var t_pitch = 0;
+	var t_amp = 0;
+	var scaleValue = this.PACKETS.length > 0 ? this.PACKETS.length : 1;
+	for (var i = 0; i < this.PACKETS.length; i++)	{
+		t_pitch += this.PACKETS[i].pitch;
+		t_amp += this.PACKETS[i].amp;
 	}
-	if (best_correlation > 0.01) {
-		// console.log("f = " + sampleRate/best_offset + "Hz (rms: " + rms + " confidence: " + best_correlation + ")")
-		return sampleRate/best_offset;
-	}
-	return -1;
-}
+	this.PITCH = Math.round(t_pitch / scaleValue);
+	this.AMP = Math.round(t_amp / scaleValue);
+};
 
-function updatePitch( time ) {
-	var cycles = new Array;
-	analyser.getByteTimeDomainData( buf );
-	var ac = autoCorrelate( buf, audioContext.sampleRate );
-	if (ac == -1) {
-		picth=0;
-		// console.log("Vague")
-	} else {
-		pitch = ac;
-		// console.log(pitch)
-		$(".pitch").css({left:pitch/100})
+Analysis.prototype.printAverages = function ()	{
+	this.$pitch.text(this.PITCH);
+	this.$amp.text(this.AMP);
+	console.log(this.AMP);
+	console.log(this.PITCH);
+};
 
-	}
-	$(".pitch").css({left:pitch/100})
 
-	if (!window.requestAnimationFrame)
-		window.requestAnimationFrame = window.webkitRequestAnimationFrame;
-	rafID = window.requestAnimationFrame( updatePitch );
-}
+Analysis.prototype.newPhrase = function ()	{
+	this.PACKETS = [];
+};
+
+var a = new Analysis();
+
+
+
+// navigator.getUserMedia = navigator.webkitGetUserMedia 
+// var audioCtx = new (window.webkitAudioContext)();
+// var voiceSelect = document.getElementById("voice");
+// var source;
+// var stream;
+// var analyser = audioCtx.createAnalyser();
+// var gainNode = audioCtx.createGain();
+// var drawVisual;
+// var buflen = 2048;
+// var buf = new Uint8Array( buflen );
+// //main block for doing the audio recording
+// function initAudioStream(){
+// 	if (navigator.getUserMedia) {
+// 		console.log('getUserMedia supported.');
+// 		navigator.getUserMedia (
+// 	  // constraints - only audio needed for this app
+// 	  {
+// 	  	audio: true
+// 	  },
+
+// 	  function(stream) {
+// 	  	source = audioCtx.createMediaStreamSource(stream);
+// 	  	source.connect(analyser);
+// 	  	gainNode.connect(audioCtx.destination);
+// 	  	visualize();
+
+// 	  	var mediaStreamSource = audioContext.createMediaStreamSource(stream);
+
+//     // Connect it to the destination.
+//     analyser = audioContext.createAnalyser();
+//     analyser.fftSize = 2048;
+//     mediaStreamSource.connect( analyser );
+//     updatePitch();
+
+
+// 	  	// toggleLiveInput()
+// 	  },
+// 	  function(err) {
+// 	  	console.log('The following gUM error occured: ' + err);
+// 	  }
+// 	  );
+// 	} else {
+// 		console.log('getUserMedia not supported on your browser!');
+// 	}
+// }
+
+
+
+
+// function visualize() {
+// 	analyser.fftSize = 256;
+// 	var bufferLength = analyser.frequencyBinCount;
+// 	console.log(bufferLength);
+// 	var dataArray = new Uint8Array(bufferLength);
+// 	function draw() {
+// 		drawVisual = requestAnimationFrame(draw);
+// 		analyser.getByteFrequencyData(dataArray);
+// 		var average=0;
+// 		for(var i = 0; i < bufferLength; i++) {
+// 			average+=dataArray[i];
+// 		}
+// 		average=average/bufferLength;
+// 		$(".volume").css({top:100-(average*1)})
+
+// 	};
+// 	draw();
+// }
+
+
+
+// /*
+// Pitch Detection
+// */
+
+
+// var audioContext = new AudioContext();
+// var isPlaying = false;
+// var sourceNode = null;
+// // var analyser = null;
+// var theBuffer = null;
+// var pitch=0;
+// //
+// var rafID = null;
+// var buflen = 2048;
+// var buf = new Uint8Array( buflen );
+// var noteStrings = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"];
+
+
+// function gotStream(stream) {
+//     // Create an AudioNode from the stream.
+//     var mediaStreamSource = audioContext.createMediaStreamSource(stream);
+
+//     // Connect it to the destination.
+//     analyser = audioContext.createAnalyser();
+//     analyser.fftSize = 2048;
+//     mediaStreamSource.connect( analyser );
+//     updatePitch();
+// }
+
+
+
+// function toggleLiveInput() {
+// 	if (isPlaying) {
+//         //stop playing and return
+//         sourceNode.stop( now );
+//         sourceNode = null;
+//         analyser = null;
+//         isPlaying = false;
+//         if (!window.cancelAnimationFrame)
+//         	window.cancelAnimationFrame = window.webkitCancelAnimationFrame;
+//         window.cancelAnimationFrame( rafID );
+//     }
+//     getUserMedia({audio:true}, gotStream);
+// }
+
+
+
+// function noteFromPitch( frequency ) {
+// 	var noteNum = 12 * (Math.log( frequency / 440 )/Math.log(2) );
+// 	return Math.round( noteNum ) + 69;
+// }
+
+// function frequencyFromNoteNumber( note ) {
+// 	return 440 * Math.pow(2,(note-69)/12);
+// }
+
+// function centsOffFromPitch( frequency, note ) {
+// 	return Math.floor( 1200 * Math.log( frequency / frequencyFromNoteNumber( note ))/Math.log(2) );
+// }
+
+
+
+// function autoCorrelate( buf, sampleRate ) {
+// 	var MIN_SAMPLES = 4;	// corresponds to an 11kHz signal
+// 	var MAX_SAMPLES = 1000; // corresponds to a 44Hz signal
+// 	var SIZE = 1000;
+// 	var best_offset = -1;
+// 	var best_correlation = 0;
+// 	var rms = 0;
+// 	var foundGoodCorrelation = false;
+
+// 	if (buf.length < (SIZE + MAX_SAMPLES - MIN_SAMPLES))
+// 		return -1;  // Not enough data
+
+// 	for (var i=0;i<SIZE;i++) {
+// 		var val = (buf[i] - 128)/128;
+// 		rms += val*val;
+// 	}
+// 	rms = Math.sqrt(rms/SIZE);
+// 	if (rms<0.01)
+// 		return -1;
+
+// 	var lastCorrelation=1;
+// 	for (var offset = MIN_SAMPLES; offset <= MAX_SAMPLES; offset++) {
+// 		var correlation = 0;
+
+// 		for (var i=0; i<SIZE; i++) {
+// 			correlation += Math.abs(((buf[i] - 128)/128)-((buf[i+offset] - 128)/128));
+// 		}
+// 		correlation = 1 - (correlation/SIZE);
+// 		if ((correlation>0.9) && (correlation > lastCorrelation))
+// 			foundGoodCorrelation = true;
+// 		else if (foundGoodCorrelation) {
+// 			// short-circuit - we found a good correlation, then a bad one, so we'd just be seeing copies from here.
+// 			return sampleRate/best_offset;
+// 		}
+// 		lastCorrelation = correlation;
+// 		if (correlation > best_correlation) {
+// 			best_correlation = correlation;
+// 			best_offset = offset;
+// 		}
+// 	}
+// 	if (best_correlation > 0.01) {
+// 		// console.log("f = " + sampleRate/best_offset + "Hz (rms: " + rms + " confidence: " + best_correlation + ")")
+// 		return sampleRate/best_offset;
+// 	}
+// 	return -1;
+// }
+
+// function updatePitch( time ) {
+// 	var cycles = new Array;
+// 	analyser.getByteTimeDomainData( buf );
+// 	var ac = autoCorrelate( buf, audioContext.sampleRate );
+// 	if (ac == -1) {
+// 		picth=0;
+// 		// console.log("Vague")
+// 	} else {
+// 		pitch = ac;
+// 		// console.log(pitch)
+// 		$(".pitch").css({left:pitch/100})
+
+// 	}
+// 	$(".pitch").css({left:pitch/100})
+
+// 	if (!window.requestAnimationFrame)
+// 		window.requestAnimationFrame = window.webkitRequestAnimationFrame;
+// 	rafID = window.requestAnimationFrame( updatePitch );
+// }
